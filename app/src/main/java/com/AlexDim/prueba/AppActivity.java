d74366a79d7b140ceb8c544845817a7481df19bf
AlexisDimasZabala <angrygiannini@gmail.com>
2021-05-14 23:22:50 -0300

    App activity configuracion

--- app/src/main/java/com/AlexDim/prueba/AppActivity.java
+++ app/src/main/java/com/AlexDim/prueba/AppActivity.java
@@ -3,13 +3,20 @@ package com.AlexDim.prueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class AppActivity extends AppCompatActivity {
 
     //declaracion de ids activity
 
+    private TextView txtResultado;
+
     private Button btnRestar, btnSumar, btnZoomMas, btnZoomMenos,btnOcultar,btnReset;
+
+    private int count=0;
+    private static int sizeText=30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,57 @@ public class AppActivity extends AppCompatActivity {
         btnZoomMenos = (Button) findViewById(R.id.btnZoomMenos);
         btnOcultar = (Button) findViewById(R.id.btnOcultar);
         btnReset = (Button) findViewById(R.id.btnReset);
+
+        txtResultado = (TextView) findViewById(R.id.txtResultado);
+
+        btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                txtResultado.setText(count);
+            }
+        });
+
+        btnRestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count--;
+                txtResultado.setText(count);
+            }
+        });
+
+        btnZoomMas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sizeText++;
+                txtResultado.setTextSize(sizeText);
+            }
+        });
+
+        btnZoomMenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sizeText--;
+                txtResultado.setTextSize(sizeText);
+            }
+        });
+
+        btnOcultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtResultado.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count=0;
+                txtResultado.setText(count);
+                txtResultado.setTextSize(30);
+                txtResultado.setVisibility(View.VISIBLE);
+            }
+        });
+
     }
 }
